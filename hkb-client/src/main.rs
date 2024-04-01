7474fb57211bd7e0a521380be578636f1d8e8890
KaloyanYosifov <kyosifovs@proton.me>
2024-04-01 12:09:29 +0200

    Tinker a bit with crossterm

--- hkb-client/src/main.rs
+++ hkb-client/src/main.rs
@@ -2,30 +2,122 @@ use crossterm::{
     cursor,
     event::{self, Event, KeyCode},
     terminal::{self, Clear},
-    QueueableCommand,
+    ExecutableCommand, QueueableCommand,
 };
 use std::panic;
 use std::{
-    io::{stdout, Write},
+    io::{stdout, Error as IOError, Write},
     time::Duration,
 };
+use thiserror::Error as ThisError;
+
+#[derive(ThisError, Debug)]
+pub enum RendererError {
+    #[error("Lexer was unable to read the next line of the file!")]
+    FailedToRenderToOutput(#[from] IOError),
+}
+
+type RenderResult = Result<(), RendererError>;
+
+#[derive(Clone, Copy)]
+struct Vector2D {
+    pub x: u16,
+    pub y: u16,
+}
+
+impl Vector2D {
+    pub fn new(x: u16, y: u16) -> Self {
+        Self { x, y }
+    }
+}
+
+struct Rectangle {
+    size: Vector2D,
+    position: Vector2D,
+}
+
+trait Renderer {
+    fn render_text(&mut self, position: Vector2D, text: impl AsRef<str>) -> RenderResult;
+    fn render(&mut self, position: Vector2D, symbol: char) -> RenderResult;
+}
+
+impl<T: QueueableCommand + Write> Renderer for T {
+    fn render_text(&mut self, position: Vector2D, text: impl AsRef<str>) -> RenderResult {
+        self.queue(cursor::MoveTo(position.x, position.y))?;
+        self.write(text.as_ref().as_bytes())?;
+
+        Ok(())
+    }
+
+    fn render(&mut self, position: Vector2D, symbol: char) -> RenderResult {
+        self.queue(cursor::MoveTo(position.x, position.y))?;
+        self.write(&[symbol as u8])?;
+
+        Ok(())
+    }
+}
+
+impl Rectangle {
+    pub fn new(position: Vector2D, size: Vector2D) -> Self {
+        Self { position, size }
+    }
+}
+
+impl Rectangle {
+    pub fn render(&self, renderer: &mut impl Renderer) -> RenderResult {
+        renderer.render_text(self.position.clone(), "-".repeat(self.size.x as usize))?;
+        renderer.render_text(
+            Vector2D {
+                x: self.position.x,
+                y: self.position.y + self.size.y,
+            },
+            "-".repeat(self.size.x as usize),
+        )?;
+
+        for y in 0..=self.size.y {
+            renderer.render(
+                Vector2D {
+                    x: self.position.x,
+                    y: y + self.position.y,
+                },
+                '|',
+            )?;
+
+            renderer.render(
+                Vector2D {
+                    x: self.position.x + self.size.x,
+                    y: y + self.position.y,
+                },
+                '|',
+            )?;
+        }
+
+        Ok(())
+    }
+}
 
 fn main() {
-    terminal::enable_raw_mode().unwrap();
-    let (width, height) = terminal::size().unwrap();
-    let mut messages = vec![];
-    let mut buffer = String::with_capacity(width as usize);
-    let mut stdout = stdout();
-    let mut should_quit = false;
-
     // Return to cooked mode when app panics
     panic::set_hook(Box::new(|e| {
         // Reenter canonical mode
         // TODO: exit raw mode
         terminal::disable_raw_mode().unwrap();
+
+        stdout().execute(terminal::LeaveAlternateScreen).unwrap();
         // Print panic info
         eprintln!("{e}");
     }));
+
+    let mut stdout = stdout();
+    let mut should_quit = false;
+
+    stdout.execute(terminal::EnterAlternateScreen).unwrap();
+    terminal::enable_raw_mode().unwrap();
+
+    let (width, height) = terminal::size().unwrap();
+    let mut buffer = String::with_capacity(width as usize);
+
+    let rectangle = Rectangle::new(Vector2D { x: 30, y: 20 }, Vector2D { x: 20, y: 5 });
 
     while !should_quit {
         while event::poll(Duration::ZERO).unwrap() {
@@ -43,7 +135,6 @@ fn main() {
                             should_quit = true;
                         }
 
-                        messages.push(buffer.clone());
                         buffer.clear();
                     }
                     KeyCode::Backspace => {
@@ -59,20 +150,32 @@ fn main() {
 
         stdout.queue(Clear(terminal::ClearType::All)).unwrap();
 
-        for (index, message) in messages.iter().enumerate() {
-            stdout.queue(cursor::MoveTo(0, index as u16)).unwrap();
-            stdout.write(message.as_bytes()).unwrap();
-        }
+        stdout
+            .render_text(
+                Vector2D {
+                    x: 0,
+                    y: height - 2,
+                },
+                "═".repeat(width as usize),
+            )
+            .unwrap();
 
-        stdout.queue(cursor::MoveTo(0, height - 2)).unwrap();
-        stdout.write("-".repeat(width as usize).as_bytes()).unwrap();
+        stdout
+            .render_text(
+                Vector2D {
+                    x: 0,
+                    y: height - 1,
+                },
+                &buffer,
+            )
+            .unwrap();
 
-        stdout.queue(cursor::MoveTo(0, height - 1)).unwrap();
-        stdout.write(buffer.as_bytes()).unwrap();
+        rectangle.render(&mut stdout);
 
         stdout.flush().unwrap();
         std::thread::sleep(Duration::from_millis(16)); // 60 FPS = 16 millis sleep
     }
 
     terminal::disable_raw_mode().unwrap();
+    stdout.execute(terminal::LeaveAlternateScreen).unwrap();
 }
