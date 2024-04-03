6a7a7e4bed15b4e71da6a6d584030e8d74903502
KaloyanYosifov <kaloqn665@gmail.com>
2024-04-03 07:26:45 +0200

    Create a main app view

---
+++ hkb-client/src/apps/main.rs
@@ -0,0 +1,46 @@
+use ratatui::prelude::{Constraint, Direction, Frame, Layout};
+use ratatui::widgets::{Block, Borders};
+
+pub struct MainApp {}
+
+impl MainApp {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl MainApp {
+    pub fn render(&mut self, frame: &mut Frame) {
+        let main_layout = Layout::new(
+            Direction::Vertical,
+            [
+                Constraint::Length(1),
+                Constraint::Min(0),
+                Constraint::Length(1),
+            ],
+        )
+        .split(frame.size());
+        frame.render_widget(
+            Block::new().borders(Borders::TOP).title("Title Bar"),
+            main_layout[0],
+        );
+        frame.render_widget(
+            Block::new().borders(Borders::TOP).title("Status Bar"),
+            main_layout[2],
+        );
+
+        let inner_layout = Layout::new(
+            Direction::Horizontal,
+            [Constraint::Percentage(50), Constraint::Percentage(50)],
+        )
+        .split(main_layout[1]);
+        frame.render_widget(
+            Block::default().borders(Borders::ALL).title("Left"),
+            inner_layout[0],
+        );
+        frame.render_widget(
+            Block::default().borders(Borders::ALL).title("Right"),
+            inner_layout[1],
+        );
+    }
+}
--- hkb-client/src/main.rs
+++ hkb-client/src/main.rs
@@ -3,6 +3,7 @@ use ratatui::{prelude::*, widgets::*};
 use std::{io::Error as IOError, time::Duration};
 use thiserror::Error as ThisError;
 
+mod apps;
 mod terminal;
 
 #[derive(ThisError, Debug)]
@@ -15,46 +16,12 @@ pub enum RendererError {
 
 type RenderResult = Result<(), RendererError>;
 
-fn ui(frame: &mut Frame) {
-    let main_layout = Layout::new(
-        Direction::Vertical,
-        [
-            Constraint::Length(1),
-            Constraint::Min(0),
-            Constraint::Length(1),
-        ],
-    )
-    .split(frame.size());
-    frame.render_widget(
-        Block::new().borders(Borders::TOP).title("Title Bar"),
-        main_layout[0],
-    );
-    frame.render_widget(
-        Block::new().borders(Borders::TOP).title("Status Bar"),
-        main_layout[2],
-    );
-
-    let inner_layout = Layout::new(
-        Direction::Horizontal,
-        [Constraint::Percentage(50), Constraint::Percentage(50)],
-    )
-    .split(main_layout[1]);
-    frame.render_widget(
-        Block::default().borders(Borders::ALL).title("Left"),
-        inner_layout[0],
-    );
-    frame.render_widget(
-        Block::default().borders(Borders::ALL).title("Right"),
-        inner_layout[1],
-    );
-}
-
 fn main() -> RenderResult {
     let mut terminal = terminal::init()?;
     let mut should_quit = false;
+    let mut main_app = apps::main::MainApp::new();
 
     terminal.clear()?;
-    let mut buffer = String::with_capacity(128);
 
     while !should_quit {
         // 30 FPS = 33 millis. Since poll is blocking we can simulate it as a sleep
@@ -65,8 +32,6 @@ fn main() -> RenderResult {
                         if c == 'c' && event.modifiers.contains(event::KeyModifiers::CONTROL) {
                             should_quit = true;
                         }
-
-                        buffer.push(c);
                     }
                     _ => {}
                 },
@@ -74,7 +39,9 @@ fn main() -> RenderResult {
             }
         }
 
-        terminal.draw(ui)?;
+        terminal.draw(|frame| {
+            main_app.render(frame);
+        })?;
     }
 
     terminal::close()?;
