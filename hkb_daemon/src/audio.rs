c6d8046895c43ae4a51a64191d51213d962a0cbe
KaloyanYosifov <kaloqn665@gmail.com>
2024-05-22 08:06:13 +0200

    #28 - Add error handling when playing audio

--- hkb_daemon/src/audio.rs
+++ hkb_daemon/src/audio.rs
@@ -1,5 +1,3 @@
-use std::borrow::Borrow;
-
 use hkb_core::logger::{debug, error};
 use parking_lot::{MappedMutexGuard, Mutex, MutexGuard};
 use thiserror::Error as ThisError;
@@ -10,8 +8,17 @@ pub enum AudioError {
     #[error("No msg channel found!")]
     FailedToFindMsgChannel,
 
+    #[error("Failed to send a message for the audio to be played!")]
+    FailedToSendAudioToMsgChannel,
+
+    #[error("Failed to find an output device to play audio on!")]
+    FailedToFindOutputDevice(#[from] rodio::StreamError),
+
     #[error("Failed to play audio!")]
-    FailedToPlayAudio,
+    FailedToPlayAudio(#[from] rodio::PlayError),
+
+    #[error("Failed to decode audio stream!")]
+    FailedToDecodeAudioStream(#[from] rodio::decoder::DecoderError),
 }
 
 static GLOBAL_AUDIO_HANDLE: Mutex<Option<AudioHandle>> = parking_lot::const_mutex(None);
@@ -53,7 +60,7 @@ impl AudioHandle {
                 Ok(())
             } else {
                 debug!(target: "DAEMON_AUDIO", "Failed to send MSG");
-                Err(AudioError::FailedToPlayAudio)
+                Err(AudioError::FailedToSendAudioToMsgChannel)
             }
         } else {
             Err(AudioError::FailedToFindMsgChannel)
@@ -63,6 +70,23 @@ impl AudioHandle {
 
 pub async fn play_audio(path: String) -> Result<(), AudioError> {
     AudioHandle::play_audio(path).await
+}
+
+async fn run_audio(path: &str) -> Result<(), AudioError> {
+    debug!(target: "DAEMON_AUDIO", "Playing audio file: {}.", path);
+
+    // TODO: add error handling
+    let (_stream, stream_handle) = rodio::OutputStream::try_default()?;
+    let sink = rodio::Sink::try_new(&stream_handle)?;
+    let file = std::io::BufReader::new(std::fs::File::open(path).unwrap());
+    let source = rodio::Decoder::new(file)?;
+
+    sink.append(source);
+    sink.sleep_until_end();
+
+    debug!(target: "DAEMON_AUDIO", "Playing of audio file {} finished!", path);
+
+    Ok(())
 }
 
 pub async fn handle(mut rx: mpsc::Receiver<String>) {
@@ -78,23 +102,17 @@ pub async fn handle(mut rx: mpsc::Receiver<String>) {
         let path = file_path.to_str().unwrap();
 
         if !file_path.exists() {
-            error!(target: "DAEMON_AUDIO", "Audio file does not exist: {}", file_path.to_str().unwrap());
+            error!(target: "DAEMON_AUDIO", "Audio file does not exist: {}", path);
 
             continue;
         }
 
-        debug!(target: "DAEMON_AUDIO", "Playing audio file: {}.", path);
-
-        // TODO: add error handling
-        let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-        let sink = rodio::Sink::try_new(&stream_handle).unwrap();
-        let file = std::io::BufReader::new(std::fs::File::open(&file_path).unwrap());
-        let source = rodio::Decoder::new(file).unwrap();
-
-        sink.append(source);
-        sink.sleep_until_end();
-
-        debug!(target: "DAEMON_AUDIO", "Playing of audio file {} finished!", path);
+        match run_audio(path).await {
+            Ok(_) => continue,
+            Err(e) => {
+                error!(target: "DAEMON_AUDIO", "Failed to play audio. Error: {}", e.to_string())
+            }
+        }
     }
 }
 
